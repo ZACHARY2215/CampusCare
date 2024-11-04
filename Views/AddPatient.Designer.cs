2ad6c854effcc6a18bab03c2a4e14d9da8659640
thj1234 <thJimenez@mcm.edu.ph>
2024-11-04 13:33:50 +0800

    Add patient, edit patient, appointment

---
+++ Views/AddPatient.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CampusCare.Views
+{
+    partial class AddPatient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonCancel = new Button();
+            buttonOk = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox4 = new TextBox();
+            comboBox1 = new ComboBox();
+            textBox5 = new TextBox();
+            comboBox2 = new ComboBox();
+            SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            buttonCancel.Location = new Point(304, 213);
+            buttonCancel.Name = "buttonCancel";
+            buttonCancel.Size = new Size(127, 54);
+            buttonCancel.TabIndex = 4;
+            buttonCancel.Text = "Cancel";
+            buttonCancel.UseVisualStyleBackColor = true;
+            buttonCancel.Click += buttonCancel_Click;
+            // 
+            // buttonOk
+            // 
+            buttonOk.Location = new Point(171, 213);
+            buttonOk.Name = "buttonOk";
+            buttonOk.Size = new Size(127, 54);
+            buttonOk.TabIndex = 3;
+            buttonOk.Text = "Ok";
+            buttonOk.UseVisualStyleBackColor = true;
+            buttonOk.Click += buttonOk_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "First name";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(118, 12);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Last name";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 70);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Gender";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(12, 41);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 128);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "Contact no.";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(118, 70);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(12, 99);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "ID";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(118, 99);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 12;
+            // 
+            // AddPatient
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(443, 279);
+            Controls.Add(comboBox2);
+            Controls.Add(textBox5);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox4);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(buttonCancel);
+            Controls.Add(buttonOk);
+            Name = "AddPatient";
+            Text = "AddPatient";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonCancel;
+        private Button buttonOk;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox4;
+        private ComboBox comboBox1;
+        private TextBox textBox5;
+        private ComboBox comboBox2;
+    }
+}
--- Views/Main.cs
+++ Views/Main.cs
@@ -70,5 +70,38 @@ namespace CampusCare.Views
             _stringFlags.LineAlignment = StringAlignment.Center;
             g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Appointment ap = new())
+            {
+                if (ap.ShowDialog() == DialogResult.OK)
+                {
+
+                }
+            }
+        }
+
+        private void buttonAddPatient_Click(object sender, EventArgs e)
+        {
+            using (AddPatient ap = new())
+            {
+                if (ap.ShowDialog() == DialogResult.OK)
+                {
+
+                }
+            }
+        }
+
+        private void buttonEditPatient_Click(object sender, EventArgs e)
+        {
+            using (EditPatient ep = new())
+            {
+                if (ep.ShowDialog() == DialogResult.OK)
+                {
+
+                }
+            }
+        }
     }
 }
