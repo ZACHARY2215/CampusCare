ee2ae872cecd1b6e27586d862e8f454a1b9ee0a0
Nokie <nokiepabualan@gmail.com>
2024-11-07 23:06:57 +0800

    login update

---
+++ Models/AdministratorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusCare.Models
+{
+    public class AdministratorModel
+    {
+        public int admin_id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
--- Views/Login.cs
+++ Views/Login.cs
@@ -1,20 +1,32 @@
+using CampusCare.ModelViews;
 using CampusCare.Views;
 
 namespace CampusCare
 {
     public partial class Login : Form
     {
+        private AdministratorMV adminMV;
+
         public Login()
         {
             InitializeComponent();
+            adminMV = new AdministratorMV();
         }
 
         public void btn_login_Click(object sender, EventArgs e)
         {
-            Main main = new();
-            this.Hide();
-            main.ShowDialog();
-            this.Show();
+
+            if (adminMV.Administrators.Exists(e => e.username.Equals(txt_username.Text) && e.password.Equals(txt_password.Text)))
+            {
+                Main main = new();
+                this.Hide();
+                main.ShowDialog();
+                this.Show();
+                errorLabel.Visible = false;
+            } else
+            {
+                errorLabel.Visible = true;
+            }
         }
     }
 }
